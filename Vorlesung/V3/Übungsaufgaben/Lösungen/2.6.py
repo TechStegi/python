0030f3fd57b483204f292a5943e0f48fcb48ab89
TechStegi <sirstegi@gmail.com>
2023-11-27 22:41:27 +0100

    Small change 2.6.py

--- Vorlesung/V3/Übungsaufgaben/Lösungen/2.6.py
+++ Vorlesung/V3/Übungsaufgaben/Lösungen/2.6.py
@@ -1,20 +1,19 @@
-
 from math import sqrt
 from math import pi
 
 
 def harmonischesMittel(a, b):
-    return (2*a*b)/(a+b)
+    return (2 * a * b) / (a + b)
+
 
 def geometrischesMittel(a, b):
-    return sqrt(a*b)
+    return sqrt(a * b)
 
-a = 3*sqrt(3)
-b = (3/2)*sqrt(3)
+
+a = 3 * sqrt(3)
+b = (3 / 2) * sqrt(3)
 
 for n in range(30):
     a = harmonischesMittel(a, b)
     b = geometrischesMittel(a, b)
-    print(n, a, b, pi-a, pi-b)
-    
-    
+    print(n, a, b, pi - a, pi - b)
