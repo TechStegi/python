990a9e87ec5fbfca5d1d5f33c975ec75d91ac831
TechStegi <sirstegi@gmail.com>
2023-12-09 09:36:24 +0100

    4.5. Vorles.aufgaben gelöst

--- Vorlesung/V3/Vorlesungsaufgaben/Selbst/4.1-4.5.py
+++ Vorlesung/V3/Vorlesungsaufgaben/Selbst/4.1-4.5.py
@@ -1,5 +1,3 @@
-
-
 file_path = "simpleText.txt"
 
 file = open(file_path, "r")
@@ -10,7 +8,7 @@ text = file.readlines()
 simple_text = open("simpleText.txt", "r").readlines()
 
 
-#4.1
+# 4.1
 def count_upper_letters():
     zeichen_count = 0
     for zeile in text:
@@ -19,10 +17,11 @@ def count_upper_letters():
                 zeichen_count += 1
     return zeichen_count
 
+
 print(count_upper_letters())
 
 
-#4.2
+# 4.2
 def count():
     a = 0
     e = 0
@@ -44,6 +43,7 @@ def count():
                 u = u + 1
     return (a, e, i, o, u)
 
+
 print(count())
 
 
@@ -54,35 +54,32 @@ def singles(some_text):
     for zeile in some_text:
         new_line = zeile[0]
         for i in range(1, len(zeile)):
-            if zeile[i] != zeile[i-1]:
+            if zeile[i] != zeile[i - 1]:
                 new_line += zeile[i]
         new_text += new_line + "\n"
     return new_text
 
+
 print(singles(simple_text))
 
 
-#4.5 funktioniert nicht
+# 4.5
 def find(insert_text):
-    x = "are"
+    x = "are st"
     count = 0
-    # index = 0
-    for zeile in insert_text: # durch jede Zeile = Array Element
-        for i in range(len(zeile)):  # durch jeden Buchstaben der Zeile
+    for zeile in insert_text:  # durch jede Zeile = Array Element
+        for i in range(len(zeile)):  # durch jedes Zeichen der Zeile
             if zeile[i] == x[0]:
                 count = 1
-                for j in range(1, len(x)):  # durch jeden Buchstaben von x
-                    if count == len(x):
-                        return "Gefunden"
-                    if i + j < len(zeile) and zeile[i+j] == x[j]:
+                for j in range(1, len(x)):  # erstes Zeichen von x wurde in insert_text an der Position gefunden, vergleiche ab da jedes Zeichen von x mit jedem Zeichen von insert_text an den Positionen i+j nach dem ersten gefunden Buchstaben
+                    if i + j < len(zeile) and zeile[i + j] == x[j]:
                         count += 1
-                        continue
                     else:
                         count = 0
                         break
+                    if count == len(x):
+                        return "Gefunden"
     return "Keine Zeichenkette x gefunden."
 
+
 print(find(simple_text))
-
-
-
